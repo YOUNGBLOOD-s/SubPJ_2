b3990de5428e22485fbd80982e4d50f3510e60a6
Heesu <jungjera2@naver.com>
2020-01-20 14:26:15 +0900

    S02P12C110-4  온습도 센서 아두이노 테스트

---
+++ EMBEDED/arduino/temperature_humidity_sensor.ino
@@ -0,0 +1,23 @@
+#include <DHT.h>
+#define DHTPIN 8   //어떤 핀에 꽂았는지
+#define DHTTYPE DHT11 //센서 이름
+DHT dht(DHTPIN, DHTTYPE); //변수에 저장
+
+
+void setup(){
+  Serial.begin(9600);
+  dht.begin();
+}
+void loop(){
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+
+  Serial.print("습도: ");
+  Serial.print(h);
+  Serial.println(" %");
+  Serial.print("온도: ");
+  Serial.print(t);
+  Serial.println(" C");
+
+  
+}
