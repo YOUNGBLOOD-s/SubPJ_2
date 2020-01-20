06e674c99c5d19484a4546b80ef9467f8d42811c
choiys <hcy3bkj@gmail.com>
2020-01-20 17:52:21 +0900

    S02P12C110-24, S02P12C110-23, S02P12C110-20 [*] Add modal

--- FRONTEND/src/components/Main/MainAD.js
+++ FRONTEND/src/components/Main/MainAD.js
@@ -1,7 +1,9 @@
-import React from "react";
-import styled from "styled-components";
-import "react-responsive-carousel/lib/styles/carousel.min.css";
-import { Carousel } from "react-responsive-carousel";
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import { Carousel } from 'react-responsive-carousel';
+import { Route, Link, withRouter } from 'react-router-dom';
+import FullScreenDialog from '../common/FullScreenDialog';
 
 const Title = styled.div`
   font-size: 2.3em;
@@ -55,24 +57,36 @@ const MainADBlock = styled.div`
 
 const datas = [
   {
-    id: "1",
-    title: "코타키나발루",
+    id: '1',
+    title: '코타키나발루',
     content:
-      "말레이시아 코타키나발루(KKUM)는 보르네오섬 북동쪽에 있는 사바(Sabah) 주의 주도로 보르네오 섬 최대의 도시입니다.",
-    img: "/static/img/코타키나발루.jpg",
-    nextURL: "http://choiys.kr"
+      '말레이시아 코타키나발루(KKUM)는 보르네오섬 북동쪽에 있는 사바(Sabah) 주의 주도로 보르네오 섬 최대의 도시입니다.',
+    img: '/static/img/코타키나발루.jpg',
+    nextURL: 'http://choiys.kr',
   },
   {
-    id: "2",
-    title: "블라디보스토크",
+    id: '2',
+    title: '블라디보스토크',
     content:
-      "블라디보스토크(러시아어: Владивосто́к)는 러시아의 도시이다. 러시아 극동의 중심지이며 프리모르스키 지방의 행정중심지이다.",
-    img: "/static/img/블라디보스토크.jpg",
-    nextURL: "http://portfolio.choiys.kr"
-  }
+      '블라디보스토크(러시아어: Владивосто́к)는 러시아의 도시이다. 러시아 극동의 중심지이며 프리모르스키 지방의 행정중심지이다.',
+    img: '/static/img/블라디보스토크.jpg',
+    nextURL: 'http://portfolio.choiys.kr',
+  },
 ];
-const MainAD = () => {
+
+const MainAD = ({ history }) => {
   const publicURL = process.env.PUBLIC_URL;
+
+  const [pid, setPid] = useState(1);
+
+  const [open, setOpen] = useState(false);
+
+  const onDoubleClick = id => {
+    // history.push(`/detail/${id}`);
+    setPid(id);
+    setOpen(true);
+  };
+
   return (
     <MainADBlock>
       <Carousel
@@ -85,8 +99,8 @@ const MainAD = () => {
         transitionTime={1000}
         interval={5000}
       >
-        {datas.map(({ title, content, img, nextURL }) => (
-          <>
+        {datas.map(({ id, title, content, img, nextURL }) => (
+          <div key={id} onDoubleClick={() => onDoubleClick(id)}>
             <div className="style">
               <div className="box">
                 <Title>{title}</Title>
@@ -98,11 +112,12 @@ const MainAD = () => {
               className="qr"
               src={`https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${nextURL}`}
             />
-          </>
+          </div>
         ))}
       </Carousel>
+      <FullScreenDialog data={datas[pid - 1]} setOpen={setOpen} open={open} />
     </MainADBlock>
   );
 };
 
-export default MainAD;
+export default withRouter(MainAD);
--- FRONTEND/src/page/AdPage.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Carousel from '../components/common/Carousel';
-
-const AdPage = () => {
-  return <Carousel />;
-};
-
-export default AdPage;
