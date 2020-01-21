b1dd899601605dca66708035b629a3537dec125a
mandaringit <mandarin.dev@kakao.com>
2020-01-21 15:02:17 +0900

    S02P12C110-60 페이지 라우트 구성

--- MANAGE_FRONTEND/src/App.js
+++ MANAGE_FRONTEND/src/App.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+import MainPage from './pages/MainPage';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+import ManagePage from './pages/ManagePage';
+import AdminPage from './pages/AdminPage';
 
 const App = () => {
-  return <div>관리자페이지</div>;
+  return (
+    <>
+      <Route component={MainPage} path="/" exact />
+      <Route component={LoginPage} path="/login" />
+      <Route component={RegisterPage} path="/register" />
+      <Route component={ManagePage} path="/manage" />
+      <Route component={AdminPage} path="/admin" />
+    </>
+  );
 };
 
 export default App;
