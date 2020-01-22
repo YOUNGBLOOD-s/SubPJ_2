eb30ebb63da0790df7712caf410b6f463f0a134a
haeun <sj05publu@naver.com>
2020-01-22 17:07:02 +0900

    delete metadata

--- BACKEND/SpringbootBackend/SpringYB/src/main/java/com/yb/rest/vo/ReqForm.java
+++
@@ -1,6 +0,0 @@
-package com.yb.rest.vo;
-
-public class ReqForm {
-	private String name, tel, email, date, text;
-	private int age;
-}
--- BACKEND/SpringbootBackend/SpringbootYoungBlood/src/main/java/com/yb/rest/controller/AdController.java
+++ BACKEND/SpringbootBackend/SpringbootYoungBlood/src/main/java/com/yb/rest/controller/AdController.java
@@ -27,11 +27,13 @@ public class AdController {
 
 	}
 	
-	/** 센서값을 받아서 데이터베이스에 저장하여 해당되는 나라 리스트를 뽑아낸다. */
+	/** 센서값을 받는다. */
 	@GetMapping("/sensor/{temp}/{hum}")
 	public void sensor(@PathVariable String temp, @PathVariable String hum) {
 		System.out.println(temp);
 		System.out.println(hum);
+		
+		// INSERT
 		
 		//두개 받아서 계산해서 나라 인덱스를 뽑아내기
 		List<Integer> nation = new LinkedList<>();
@@ -45,29 +47,24 @@ public class AdController {
 	/** 센서값을 받아 거기에 맞는 추천 나라를 객체 배열로 전송한다. */
 	public void selectnation(List<Integer> nation) {
 		List<Sendtofront> Countrylist = new LinkedList<>();
-//		for(int idx=0; idx<nation.size(); idx++) {
-//			int id = Countrylist.get(idx).getId();
-//			List<String> imgs = ser.getImgs(id);
-//			List<String> modalContents = ser.getModalcontents(id);
-//			
-//			//avertb
-//			float humid = 0;
-//			float temp = 0;
-//			
-//			//nationtb
-//			String name = null;
-//			String thumbnail = null;
-//			
-//			//contents
-//			String speechtext = null;
-//			
-//			Countrylist.add(new Sendtofront (id, temp, humid, name, speechtext, thumbnail, imgs, modalContents));
-//		}
-		
-		//only test
-		//List<String> imgs = ser.getImgs(1);
-		List<String> modal = ser.getModalcontents(1);
-		System.out.println(modal);
+		for(int idx=0; idx<nation.size(); idx++) {
+			int id = Countrylist.get(idx).getId();
+			List<String> imgs = ser.getImgs(id);
+			List<String> modalContents = ser.getModalcontents(id);
+			
+			//avertb
+			float humid = 0;
+			float temp = 0;
+			
+			//nationtb
+			String name = null;
+			String thumbnail = null;
+			
+			//contents
+			String speechtext = null;
+			
+			Countrylist.add(new Sendtofront (id, temp, humid, name, speechtext, thumbnail, imgs, modalContents));
+		}
 		
 		//json 형식으로 바꾸고
 		//프론트로 전송하기
