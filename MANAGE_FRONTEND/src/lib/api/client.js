37b35218b837d29cb1de70ad87123537df883a07
mandaringit <mandarin.dev@kakao.com>
2020-01-22 17:47:22 +0900

    API 함수 작성

--- MANAGE_FRONTEND/src/lib/api/client.js
+++ MANAGE_FRONTEND/src/lib/api/client.js
@@ -4,3 +4,14 @@ const client = axios.create();
 
 // API 주소를 다른 곳으로 사용함
 // client.defaults.baseURL = ''
+
+// 로그인
+export const login = ({ username, password }) =>
+  client.post('/api/auth/login', { username, password });
+
+// 회원가입
+export const register = ({ username, password }) =>
+  client.post('/api/auth/register', { username, password });
+
+// 로그인 상태 확인
+export const check = () => client.get('/api/auth/check');
