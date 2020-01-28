b1f83b400909a119f28d3adffcff371a10d82ab8
haeun <sj05publu@naver.com>
2020-01-28 15:24:29 +0900

    S02P12C110-94 Backend Springboot member update

--- BACKEND/SpringbootBackend/SpringbootYoungBlood/src/main/java/com/yb/rest/dao/IMemDao.java
+++ BACKEND/SpringbootBackend/SpringbootYoungBlood/src/main/java/com/yb/rest/dao/IMemDao.java
@@ -1,5 +1,18 @@
 package com.yb.rest.dao;
 
+import java.util.ArrayList;
+
+import com.yb.rest.vo.Member;
+
 public interface IMemDao {
 	public String getPassword(String username);
+	public boolean registerMem(String username, String password, String company, int grade);
+
+	public ArrayList<Member> listMem();
+
+	public Member InfoMem(String username);
+
+	public boolean UpdateMem(String username, String password, String company, int grade);
+
+	public boolean DeleteMem(String password);
 }
