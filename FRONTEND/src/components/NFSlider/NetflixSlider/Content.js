6217178a2e85ad6503bc525fc397f1cc986c64f5
choiys <hcy3bkj@gmail.com>
2020-01-28 15:57:32 +0900

    S02P12C110-105 [*] content.js return형으로 변경

--- FRONTEND/src/components/NFSlider/NetflixSlider/Content.js
+++ FRONTEND/src/components/NFSlider/NetflixSlider/Content.js
@@ -3,37 +3,44 @@ import IconCross from './../Icons/IconCross';
 import './Content.scss';
 import WbSunnyIcon from '@material-ui/icons/WbSunny';
 import OpacityIcon from '@material-ui/icons/Opacity';
+import styled from 'styled-components';
 
-const Content = ({ movie, onClose }) => (
-  <div className="content">
-    <div className="content__background">
-      <div className="content__background__shadow" />
-      <div
-        className="content__background__image"
-        style={{ backgroundImage: `url(${movie.imageBg})` }}
-      />
-    </div>
-    <div className="content__area">
-      <div className="content__area__container">
-        <div className="content__title">{movie.title}</div>
-        <div className="content__description">
-          <h2>
-            <WbSunnyIcon /> 17&deg;C ~ -3&deg;C&nbsp;&nbsp;&nbsp;
-          </h2>
-          <h2>
-            <OpacityIcon /> 0 %
-          </h2>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque
-          et euismod ligula. Morbi mattis pretium eros, ut mollis leo tempus
-          eget. Sed in dui ac ipsum feugiat ultricies. Phasellus vestibulum enim
-          quis quam congue, non fringilla orci placerat. Praesent sollicitudin
-        </div>
+const Content = ({ movie, onClose }) => {
+  const Weather = styled.h2`
+    color: white;
+  `;
+  return (
+    <div className="content">
+      <div className="content__background">
+        <div className="content__background__shadow" />
+        <div
+          className="content__background__image"
+          style={{ backgroundImage: `url(${movie.imageBg})` }}
+        />
       </div>
-      {/* <button className="content__close" onClick={onClose}>
+      <div className="content__area">
+        <div className="content__area__container">
+          <div className="content__title">{movie.title}</div>
+          <div className="content__description">
+            <Weather>
+              <WbSunnyIcon /> 17&deg;C ~ -3&deg;C&nbsp;&nbsp;&nbsp;
+            </Weather>
+            <Weather>
+              <OpacityIcon /> 0 %
+            </Weather>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Pellentesque et euismod ligula. Morbi mattis pretium eros, ut mollis
+            leo tempus eget. Sed in dui ac ipsum feugiat ultricies. Phasellus
+            vestibulum enim quis quam congue, non fringilla orci placerat.
+            Praesent sollicitudin
+          </div>
+        </div>
+        {/* <button className="content__close" onClick={onClose}>
         <IconCross />
       </button> */}
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Content;
