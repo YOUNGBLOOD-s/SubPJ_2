22a6a58a1a1da4f81294157a9814af4f652a730a
haeun <sj05publu@naver.com>
2020-01-28 09:54:33 +0900

    S02P12C110-88

--- BACKEND/SpringbootBackend/SpringbootYoungBlood/src/main/java/com/yb/rest/controller/MemberController.java
+++ BACKEND/SpringbootBackend/SpringbootYoungBlood/src/main/java/com/yb/rest/controller/MemberController.java
@@ -1,5 +1,6 @@
 package com.yb.rest.controller;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -14,37 +15,42 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.yb.rest.vo.Member;
 
+import io.jsonwebtoken.Jwts;
 
 @CrossOrigin
 @RestController
 @RequestMapping("/api")
 public class MemberController {
+	
+//	public static String createTocken() {
+//		String key = "A";
+//		Map<String, Object> headers = new HashMap<>();
+//		headers.compute(key, remappingFunction)
+//	}
 
-	/**회원가입*/
+	/** 회원가입 */
 	@PostMapping("/auth/register")
 	public ResponseEntity<Map<String, Object>> meminsert(@RequestBody Member reg) {
 		ResponseEntity<Map<String, Object>> res = null;
-		
-		//토큰 생성하기
-		
-		
-		
+
+		// 토큰 생성하기
+
 		Map<String, Object> msg = new HashMap<String, Object>();
-		msg.put("username", reg.getUsername()); //혁준오빠한테 {test: 0} 으로 전송됨
-		msg.put("company", reg.getCompany()); 
-		//msg.put("token", jws);
+		msg.put("username", reg.getUsername()); // 혁준오빠한테 {test: 0} 으로 전송됨
+		msg.put("company", reg.getCompany());
+		// msg.put("token", jws);
 		res = new ResponseEntity<Map<String, Object>>(msg, HttpStatus.OK);
 		return res;
 	}
-	
-	/**로그인*/
+
+	/** 로그인 */
 	@PostMapping("/auth/login")
 	public ResponseEntity<Map<String, Object>> memlogin(@RequestBody Member login) {
 		System.out.println(login.toString());
 		return null;
 	}
-	
-	/**로그인 상태 확인*/
+
+	/** 로그인 상태 확인 */
 	@PostMapping("/auth/check")
 	public ResponseEntity<Map<String, Object>> memloginfo() {
 		System.out.println("zzz");
