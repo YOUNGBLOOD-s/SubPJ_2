16d485fe2f0a7562f2d112a8d540b92e0c9c5efc
choiys <hcy3bkj@gmail.com>
2020-02-03 11:20:47 +0900

    S02P13C110-41 [*] 관리페이지 탭/메인 연동

--- MANAGE_FRONTEND/src/pages/MainPage.js
+++ MANAGE_FRONTEND/src/pages/MainPage.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ButtonAppbarContainer from '../containers/common/ButtonAppbarContainer';
+import ManagePage from './ManagePage';
 
 const MainPage = () => {
   return (
     <>
       <ButtonAppbarContainer />
+      <ManagePage />
     </>
   );
 };
