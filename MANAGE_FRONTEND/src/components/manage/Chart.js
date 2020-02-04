8daeb6d78755c0f59abef1c1e9030e395d1b11cc
choiys <hcy3bkj@gmail.com>
2020-02-04 09:56:43 +0900

    I don't know what is changed

--- MANAGE_FRONTEND/src/components/manage/Chart.js
+++ MANAGE_FRONTEND/src/components/manage/Chart.js
@@ -95,15 +95,58 @@ const data = [
   },
 ];
 
-export default class Example extends PureComponent {
-  static jsfiddleUrl = 'https://jsfiddle.net/alidingling/2vyv694u/';
+const data2 = [
+  {
+    name: 'Page A',
+    uv: 4000,
+    pv: 2400,
+    amt: 2400,
+  },
+  {
+    name: 'Page B',
+    uv: 3000,
+    pv: 1398,
+    amt: 2210,
+  },
+  {
+    name: 'Page C',
+    uv: 2000,
+    pv: 9800,
+    amt: 2290,
+  },
+  {
+    name: 'Page D',
+    uv: 2780,
+    pv: 3908,
+    amt: 2000,
+  },
+  {
+    name: 'Page E',
+    uv: 1890,
+    pv: 4800,
+    amt: 2181,
+  },
+  {
+    name: 'Page F',
+    uv: 2390,
+    pv: 3800,
+    amt: 2500,
+  },
+  {
+    name: 'Page G',
+    uv: 3490,
+    pv: 4300,
+    amt: 2100,
+  },
+];
 
+export default class Example extends PureComponent {
   render() {
     return (
       <div>
         <h4>Show Count</h4>
         <AreaChart
-          width={500}
+          width={data.length * 100}
           height={200}
           data={data}
           syncId="anyId"
@@ -122,9 +165,9 @@ export default class Example extends PureComponent {
         </AreaChart>
         <h4>QR View Count</h4>
         <AreaChart
-          width={500}
+          width={data2.length * 100}
           height={200}
-          data={data}
+          data={data2}
           syncId="anyId"
           margin={{
             top: 10,
