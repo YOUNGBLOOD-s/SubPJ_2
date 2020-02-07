131d1249abf9d68ffb1f5962ae5dd2736626e40e
haeun <sj05publu@naver.com>
2020-02-07 15:51:35 +0900

    망할 토큰 진짜 -_-

--- BACKEND/SpringbootBackend/SpringbootYoungBlood/src/main/java/com/yb/rest/controller/ManageController.java
+++ BACKEND/SpringbootBackend/SpringbootYoungBlood/src/main/java/com/yb/rest/controller/ManageController.java
@@ -22,7 +22,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.yb.rest.service.IAdService;
 import com.yb.rest.service.IManService;
-import com.yb.rest.vo.Counsel;
 import com.yb.rest.vo.Image;
 import com.yb.rest.vo.Monthtb;
 import com.yb.rest.vo.Nation;
@@ -46,12 +45,13 @@ public class ManageController {
 	public void ExceptionMethod(Exception e) {
 
 	}
+	
+	//token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1ODA0NTU4MzY2ODQsInVzZXJuYW1lIjoiYWRtaW4ifQ.hstghy7DypqOI3wj2-7trxtpgps3VvzAvD1ri9deLl4";
 
 	/** 사용자 상품 전체보기 */
-	@GetMapping("/man/nation/list")
+	@GetMapping("/man/nation/list/")
 	@ApiOperation(value = "광고주의 등록 상품(nation) 전체 리스트")
 	public ResponseEntity<Map<String, Object>> nationList(@RequestHeader(value = "Authorization") String token) {
-		token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1ODA0NTU4MzY2ODQsInVzZXJuYW1lIjoiYWRtaW4ifQ.hstghy7DypqOI3wj2-7trxtpgps3VvzAvD1ri9deLl4";
 		ResponseEntity<Map<String, Object>> res = null;
 		Map<String, Object> msg = new HashMap<String, Object>();
 		ArrayList<Nation> list = null;
@@ -61,7 +61,7 @@ public class ManageController {
 			String username = (String) de.get("username");
 			int customer = ser.getIdx(username);
 			int grade = ser.searchGrade(customer);
-			
+
 			if (grade == 1) {
 				list = ser.nationListAll(customer);
 			} else if (grade >= 2) {
@@ -84,10 +84,8 @@ public class ManageController {
 	@ApiOperation(value = "광고주의 등록 상품정보 보기 서비스.")
 	public ResponseEntity<Map<String, Object>> nationInfo(@RequestHeader(value = "Authorization") String token,
 			@PathVariable("idx") int idx) {
-		token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1ODA0NTU4MzY2ODQsInVzZXJuYW1lIjoiYWRtaW4ifQ.hstghy7DypqOI3wj2-7trxtpgps3VvzAvD1ri9deLl4";
 		ResponseEntity<Map<String, Object>> res = null;
 		Map<String, Object> msg = new HashMap<String, Object>();
-		System.out.println(idx);
 		Nation nat = new Nation();
 		Monthtb mon = new Monthtb();
 		ArrayList<Route> contentsList = null;
@@ -97,21 +95,15 @@ public class ManageController {
 			msg.put("username", de.get("username"));
 			String username = (String) de.get("username");
 			int customer = ser.getIdx(username);
-			int grade = ser.searchGrade(customer);
-			if (grade == 1) {
-				//관리자는 다 볼 수 있고...
-				
-			} else if (grade >= 2) {
-				// idx로 nationtb에서 selectOne
-				nat = ser.nationInfo(idx);
-				
-				// monthtb에 nation=#{idx}로 selectOne
-				mon = ser.monthInfo(idx);
-				
-				// img랑 contents는 selectList로 받아와야하고.
-				contentsList = ser.contentsInfo(idx);
-				imageList = ser.imagesInfo(idx);				
-			} 
+			// idx로 nationtb에서 selectOne
+			nat = ser.nationInfo(idx);
+
+			// monthtb에 nation=#{idx}로 selectOne
+			mon = ser.monthInfo(idx);
+
+			// img랑 contents는 selectList로 받아와야하고.
+			contentsList = ser.contentsInfo(idx);
+			imageList = ser.imagesInfo(idx);
 
 			msg.put("resmsg", "조회성공");
 			msg.put("natValue", nat);
@@ -140,19 +132,27 @@ public class ManageController {
 			String username = (String) de.get("username");
 			int customer = ser.getIdx(username);
 			int grade = ser.searchGrade(customer);
+
 			if (grade > 0) {
 				boolean resProduct = ser.nationinsert(nat.getEn_name(), nat.getKo_name(), nat.getDust(),
-						nat.getContinents(), customer + "", nat.getWeight(), nat.getSpeech(),
+						nat.getContinents(),customer + "", nat.getWeight(), nat.getSpeech(),
 						nat.getPrice(), nat.getS_date(), nat.getF_date());
+				int last = Integer.MIN_VALUE;
+				List<Nation> list = ser.nationList(customer);
+				for(int i=0; i<list.size(); i++) {
+					last = Math.max(last, Integer.parseInt(list.get(i).getIdx()));
+				}
 				msg.put("resmsg", "등록성공");
 				msg.put("resvalue", resProduct);
+				msg.put("nationidx", last);
 				res = new ResponseEntity<Map<String, Object>>(msg, HttpStatus.OK);
 			} else {
 				msg.put("resmsg", "권한없음.");
 				res = new ResponseEntity<Map<String, Object>>(msg, HttpStatus.UNAUTHORIZED);
 			}
 		} catch (Exception e) {
-			msg.put("resmsg", e.getMessage());
+			msg.put("resmsg", "등록실패");
+			System.out.println(e.getMessage());
 			res = new ResponseEntity<Map<String, Object>>(msg, HttpStatus.UNAUTHORIZED);
 		}
 
@@ -160,10 +160,9 @@ public class ManageController {
 	}
 
 	/** 사용자 상품 삭제 */
-	@DeleteMapping("/man/nation/delete/{nation}")
+	@DeleteMapping("/man/nation/delete/")
 	@ApiOperation(value = "매니저가 nationtb삭제 (cascade).")
-	public ResponseEntity<Map<String, Object>> nationDelete(@RequestHeader(value = "Authorization") String token,
-			@PathVariable("nation") int nation) {
+	public ResponseEntity<Map<String, Object>> nationDelete(@RequestHeader(value = "Authorization") String token) {
 		ResponseEntity<Map<String, Object>> res = null;
 		Map<String, Object> msg = new HashMap<String, Object>();
 		try {
@@ -174,7 +173,7 @@ public class ManageController {
 			int grade = ser.searchGrade(customer);
 
 			if (grade == 1) {
-				boolean resDelete = ser.nationdelete(nation);
+				boolean resDelete = ser.nationdelete(customer);
 				msg.put("resmsg", "삭제성공");
 				msg.put("resvalue", resDelete);
 				res = new ResponseEntity<Map<String, Object>>(msg, HttpStatus.OK);
