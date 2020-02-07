26304fd06c5d755acfdcceed6c8e84e46e8035c6
mandaringit <mandarin.dev@kakao.com>
2020-02-07 17:49:36 +0900

    stepper 모듈 initialize 액션 추가

--- MANAGE_FRONTEND/src/components/manage/productManage/AddProduct/AddProduct.js
+++ MANAGE_FRONTEND/src/components/manage/productManage/AddProduct/AddProduct.js
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ProductStepper from './ProductStepper';
+import { useDispatch } from 'react-redux';
+import { initializeStep } from '../../../../modules/stepper';
 
 const AddProduct = () => {
+  const dispatch = useDispatch();
+  // 단계 초기화
+  useEffect(() => {
+    dispatch(initializeStep());
+  }, [dispatch]);
+
   return (
     <div>
       <ProductStepper />
--- MANAGE_FRONTEND/src/modules/stepper.js
+++ MANAGE_FRONTEND/src/modules/stepper.js
@@ -1,19 +1,24 @@
 import { createAction, handleActions } from 'redux-actions';
 
+const INITIALIZE_STEP = 'stepper/INITIALIZE_STEP';
 const NEXT_STEP = 'stepper/NEXT_STEP';
 const PREV_STEP = 'stepper/PREV_STEP';
 const RESET_STEP = 'stepper/RESET_STEP';
 
+export const initializeStep = createAction(INITIALIZE_STEP, step => step);
 export const nextStep = createAction(NEXT_STEP, step => step);
 export const prevStep = createAction(PREV_STEP, step => step);
 export const resetStep = createAction(RESET_STEP, step => step);
 
 const initialState = {
-  step: 1,
+  step: 0,
 };
 
 const stepper = handleActions(
   {
+    [INITIALIZE_STEP]: () => ({
+      step: 0,
+    }),
     [NEXT_STEP]: state => ({
       step: state.step + 1,
     }),
