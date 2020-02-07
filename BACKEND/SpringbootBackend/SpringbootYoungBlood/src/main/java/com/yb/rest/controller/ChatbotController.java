d24b899a3ea388ab7127416142a4a0d75ea6d3fc
Heesu <jungjera2@naver.com>
2020-02-07 10:48:39 +0900

    S02P13C110-8 chatbot function final

--- BACKEND/SpringbootBackend/SpringbootYoungBlood/src/main/java/com/yb/rest/controller/ChatbotController.java
+++ BACKEND/SpringbootBackend/SpringbootYoungBlood/src/main/java/com/yb/rest/controller/ChatbotController.java
@@ -1,6 +1,8 @@
 package com.yb.rest.controller;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
@@ -137,11 +139,12 @@ public class ChatbotController {
 		return map;
 	}
 
-	class date{
+	class date {
 		private String idx;
-		private String  s_date;
+		private String s_date;
 		private String f_date;
 		private String url;
+
 		public date(String idx, String s_date, String f_date, String url) {
 			super();
 			this.idx = idx;
@@ -149,29 +152,62 @@ public class ChatbotController {
 			this.f_date = f_date;
 			this.url = url;
 		}
+
+		@Override
+		public String toString() {
+			return "date [idx=" + idx + ", s_date=" + s_date + ", f_date=" + f_date + ", url=" + url + "]";
+		}
+
 	}
-	
+
 	@PostMapping("/latest")
 	public Map<String, Object> latest() {
 		Map<String, Object> map = new HashMap<String, Object>();
 
 		List<Nation> li = nationSer.selectNations();
-		List<date> s_dateLi=new ArrayList<date>();
-		System.out.println();
+		List<date> s_dateLi = new ArrayList<date>();
+
+		// 오늘 월,일 가져오기
+		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+		Calendar time = Calendar.getInstance();
+		String today = format.format(time.getTime());
+		int nmonth = Integer.parseInt(today.substring(5, 7));
+		int nday = Integer.parseInt(today.substring(8, 10));
+
 		for (int i = 0; i < li.size(); i++) {
-			s_dateLi.add(new date(li.get(i).getIdx(),li.get(i).getS_date(),li.get(i).getF_date(),li.get(i).getUrl()));
+			String compareStr = li.get(i).getS_date();
+			int comMonth = Integer.parseInt(compareStr.substring(5, 7));
+			int comDay = Integer.parseInt(compareStr.substring(8, 10));
+			System.out.println(nmonth+" "+ comMonth+"/"+nday +" "+ comDay);
+			if (nmonth <= comMonth && nday <= comDay) {
+				s_dateLi.add(new date(li.get(i).getIdx(), li.get(i).getS_date(), li.get(i).getF_date(), li.get(i).getUrl()));
+			}
 		}
-		
-		Collections.sort(s_dateLi,new Comparator<date>() {
+
+		Collections.sort(s_dateLi, new Comparator<date>() {
 
 			@Override
 			public int compare(date o1, date o2) {
 				// TODO Auto-generated method stub
-				return 0;
+				return o1.s_date.compareTo(o2.s_date);
 			}
 		});
-		
+
+		for (int i = 0; i < s_dateLi.size(); i++) {
+			System.out.println(s_dateLi.get(i).toString());
+		}
+		/*
+		 * sb.append(tmp.getKo_name()).append("\n");
+		 * sb.append("http://52.78.218.79:8282/detail/" + tmp.getIdx()).append("\n");
+		 */
+		Nation resul=nationSer.getNationdetail(Integer.parseInt(s_dateLi.get(0).idx));
 		StringBuilder sb = new StringBuilder();
+		sb.append(resul.getKo_name()+"\n");
+		sb.append("출발일자: "+s_dateLi.get(0).s_date+"\n");
+		sb.append("도착일자: "+s_dateLi.get(0).f_date+"\n\n");
+		
+		sb.append("http://52.78.218.79:8282/detail/" +s_dateLi.get(0).idx);
+		
 		map.put("result", sb);
 		return map;
 	}
