072a30990986cf23d4dd4338c0d52ba48fa55269
mandaringit <mandarin.dev@kakao.com>
2020-02-08 20:56:23 +0900

    ImageUploader 일단 구현

---
+++ MANAGE_FRONTEND/src/components/common/ImageUploader.js
@@ -0,0 +1,60 @@
+import React, { useState, useRef } from 'react';
+import AWS from 'aws-sdk';
+
+const ImageUploader = ({ setImageUrl }) => {
+  const [file, setFile] = useState(null);
+  const UploadInput = useRef(null);
+  const albumBucketName = process.env.REACT_APP_AWS_BUCKET_NAME;
+  const bucketRegion = process.env.REACT_APP_AWS_REGION;
+  const IdentityPoolId = process.env.REACT_APP_AWS_POOLID;
+
+  const upload = () => {
+    AWS.config.update({
+      region: bucketRegion,
+      credentials: new AWS.CognitoIdentityCredentials({
+        IdentityPoolId: IdentityPoolId,
+      }),
+    });
+
+    const timestamp = new Date().getTime();
+    const photoKey = `uploaded/${timestamp + file.name}`;
+    const uploaded = new AWS.S3.ManagedUpload({
+      params: {
+        Bucket: albumBucketName,
+        Key: photoKey,
+        Body: file,
+        ACL: 'public-read',
+      },
+    });
+
+    const promise = uploaded.promise();
+    promise
+      .then(data => {
+        const { Location, key, Bucket } = data;
+        console.log(Location);
+        // setImageUrl(Location) //위에서 set받아서 수행
+      })
+      .catch(err => console.log(err));
+  };
+
+  const handleFileUpload = () => {
+    setFile(UploadInput.current.files[0]);
+  };
+
+  return (
+    <div>
+      <input
+        ref={UploadInput}
+        type="file"
+        onChange={handleFileUpload}
+        accept=".jpg,.jpeg,.png,.gif"
+      />
+      <label className="custom-file-label" htmlFor="inputGroupFile04">
+        {file ? file.name : '이미지를 선택해주세요'}
+      </label>
+      <button onClick={upload}>업로드</button>
+    </div>
+  );
+};
+
+export default ImageUploader;
--- MANAGE_FRONTEND/src/components/manage/productManage/ProductManage.js
+++ MANAGE_FRONTEND/src/components/manage/productManage/ProductManage.js
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import MyProducts from './MyProducts/MyProducts';
 import AddProduct from './AddProduct/AddProduct';
+import ImageUploader from '../../common/ImageUploader';
 
 const ProductManageWrapper = styled.div`
   max-width: 1000px;
@@ -12,6 +13,8 @@ const ProductManage = () => {
   return (
     <div>
       <h1>제품 관리</h1>
+      <h4>이미지 업로드 테스트</h4>
+      <ImageUploader />
       <h2>광고주가 광고하고 있는 제품의</h2>
       <h2>목록보기, 항목 삭제하기, 항목 수정하기, 항목 추가하기</h2>
       <ProductManageWrapper>
