e847670d3125f1857520aa50905754f74866aa61
mandaringit <mandarin.dev@kakao.com>
2020-02-10 15:11:54 +0900

    S02P13C110-80 ADMIN이 아니면 ADMINPAGE 접근 불가

--- MANAGE_FRONTEND/src/pages/AdminPage.js
+++ MANAGE_FRONTEND/src/pages/AdminPage.js
@@ -1,24 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ButtonAppbarContainer from '../containers/common/ButtonAppbarContainer';
+import MyProducts from '../components/manage/productManage/MyProducts/MyProducts';
+import { useSelector } from 'react-redux';
 // import { useSelector } from 'react-redux';
 // import { useEffect } from 'react';
 
-const AdminPage = () => {
-  // const { user } = useSelector(({ user }) => ({
-  //   user: user.user,
-  // }));
+const AdminPage = ({ history }) => {
+  const { user } = useSelector(({ user }) => ({
+    user: user.user,
+  }));
 
-  // FIXME: admin이 아니면 들어올수 없게 만들어야합니다.
-  // useEffect(() => {
-  //   if (user.username !== 'admin') {
-  //     history.push('/');
-  //   }
-  // }, [user, history]);
+  // ADMIN이 아니면 못들어옵니다.
+  useEffect(() => {
+    if (!user || user.username !== 'admin') {
+      history.push('/');
+    }
+  }, [user, history]);
 
   return (
     <div>
       <ButtonAppbarContainer />
-      <h1>우리들의 ! 어드민 페이지</h1>
+      <MyProducts />
     </div>
   );
 };
