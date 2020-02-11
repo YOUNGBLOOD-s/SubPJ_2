c007f4a4c5ea608a2818495a416d612f9a1cf7a9
mandaringit <mandarin.dev@kakao.com>
2020-02-11 10:44:26 +0900

    상품 추가 페이지 최대넓이 제한

--- MANAGE_FRONTEND/src/components/manage/productManage/AddProduct/AddProduct.js
+++ MANAGE_FRONTEND/src/components/manage/productManage/AddProduct/AddProduct.js
@@ -3,19 +3,29 @@ import ProductStepper from './ProductStepper';
 import { useDispatch } from 'react-redux';
 import { initializeStep } from '../../../../modules/stepper';
 import { initializeProduct } from '../../../../modules/product';
+import styled from 'styled-components';
+
+const AddProductContainer = styled.div``;
+
+const AddProductWrapper = styled.div`
+  max-width: 1000px;
+  margin: 0 auto;
+`;
 
 const AddProduct = () => {
   const dispatch = useDispatch();
   // TODO: 모든 단계가 완료되었다면 컴포넌트 시작시 초기화.. 에러 안나는지 확인
   useEffect(() => {
-    // dispatch(initializeStep());
-    // dispatch(initializeProduct());
+    dispatch(initializeStep());
+    dispatch(initializeProduct());
   }, [dispatch]);
 
   return (
-    <div>
-      <ProductStepper />
-    </div>
+    <AddProductContainer>
+      <AddProductWrapper>
+        <ProductStepper />
+      </AddProductWrapper>
+    </AddProductContainer>
   );
 };
 
