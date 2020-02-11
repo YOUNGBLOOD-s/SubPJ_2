5c7f10bc0a39c1158b4f0a99716efb72679681fe
mandaringit <mandarin.dev@kakao.com>
2020-02-11 10:45:48 +0900

    어드민 페이지 라우팅

--- MANAGE_FRONTEND/src/pages/AdminPage.js
+++ MANAGE_FRONTEND/src/pages/AdminPage.js
@@ -3,8 +3,7 @@ import ButtonAppbarContainer from '../containers/common/ButtonAppbarContainer';
 import MyProducts from '../components/manage/productManage/MyProducts/MyProducts';
 import { useSelector } from 'react-redux';
 import AddProduct from '../components/manage/productManage/AddProduct/AddProduct';
-// import { useSelector } from 'react-redux';
-// import { useEffect } from 'react';
+import { Route, Link } from 'react-router-dom';
 
 const AdminPage = ({ history }) => {
   const { user } = useSelector(({ user }) => ({
@@ -21,8 +20,14 @@ const AdminPage = ({ history }) => {
   return (
     <div>
       <ButtonAppbarContainer />
-      <MyProducts />
-      <AddProduct />
+      <div>
+        <Link to="/admin">광고목록</Link>
+        <Link to="/admin/users">유저목록</Link>
+        <Link to="/admin/add">상품추가</Link>
+        <Link to="/admin/update">상품수정</Link>
+      </div>
+      <Route component={MyProducts} path="/admin" exact />
+      <Route component={AddProduct} path="/admin/add" />
     </div>
   );
 };
