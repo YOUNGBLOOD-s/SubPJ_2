cff37d0d8dc4c9d2e500dbcd8f81d13d1191d2d9
mandaringit <mandarin.dev@kakao.com>
2020-02-11 10:45:33 +0900

    내 상품 보기 정렬 및 간단한 스타일링

--- MANAGE_FRONTEND/src/components/manage/productManage/MyProducts/MyProduct.js
+++ MANAGE_FRONTEND/src/components/manage/productManage/MyProducts/MyProduct.js
@@ -8,27 +8,21 @@ import CardContent from '@material-ui/core/CardContent';
 import { remove_list } from '../../../../lib/api/ad';
 import { removeAd } from '../../../../modules/ads';
 import DeleteAlertDialog from '../../../common/DeleteAlertDialog';
+import styled from 'styled-components';
 
 const useStyles = makeStyles({
   root: {
     minWidth: 275,
   },
-  bullet: {
-    display: 'inline-block',
-    margin: '0 2px',
-    transform: 'scale(0.8)',
-  },
-  title: {
-    fontSize: 14,
-  },
-  pos: {
-    marginBottom: 12,
-  },
 });
+
+const ContentWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
 
 const MyProduct = ({ ad }) => {
   const classes = useStyles();
-  console.log(ad);
   const { idx, ko_name, en_name, s_date, f_date, price, customer } = ad;
   const { user } = useSelector(({ user }) => ({
     user: user.user,
@@ -53,15 +47,23 @@ const MyProduct = ({ ad }) => {
     <Card className={classes.root} variant="outlined">
       <CardContent>
         <component.Grid container>
-          <div>
-            {idx} : {ko_name}
-          </div>
+          <ContentWrapper>
+            <div>ID : {idx}</div>
+            <div>
+              {ko_name} ({en_name})
+            </div>
+            <div>출발 : {s_date}</div>
+            <div>도착 : {f_date}</div>
+            <div>가격 : {price}</div>
+            <div>광고주 : {customer}</div>
+          </ContentWrapper>
         </component.Grid>
       </CardContent>
       {user && user.username === 'admin' && (
         <CardActions>
           <component.Button onClick={onUpdateAd}>수정</component.Button>
           <DeleteAlertDialog>
+            {/* children으로 삭제 버튼 넣어줌 */}
             <component.Button onClick={onRemoveAd} color="secondary">
               삭제
             </component.Button>
--- MANAGE_FRONTEND/src/components/manage/productManage/MyProducts/MyProducts.js
+++ MANAGE_FRONTEND/src/components/manage/productManage/MyProducts/MyProducts.js
@@ -3,8 +3,16 @@ import styled from 'styled-components';
 import MyProduct from './MyProduct';
 import { useDispatch, useSelector } from 'react-redux';
 import { listAds } from '../../../../modules/ads';
+import component from '../../../../lib/material/component';
+import TitleBar from '../../../Detail/common/TitleBar';
 
-const MyProductsWrapper = styled.div``;
+const MyProductsWrapper = styled.div`
+  padding: 1rem;
+`;
+
+const AdListWraaper = styled.div`
+  margin: 1rem 0;
+`;
 
 const MyProducts = () => {
   const dispatch = useDispatch();
@@ -25,14 +33,30 @@ const MyProducts = () => {
 
   return (
     <MyProductsWrapper>
-      <h1>AD LIST</h1>
+      <TitleBar>광고 목록</TitleBar>
       {!loading && ads && (
-        <>
-          {ads.map(ad => (
-            <MyProduct ad={ad} key={ad.idx} />
-          ))}
-        </>
+        <AdListWraaper>
+          <component.Grid container spacing={1}>
+            {ads.map(ad => (
+              <component.Grid item xs={6} md={4} key={ad.idx}>
+                <MyProduct ad={ad} />
+              </component.Grid>
+            ))}
+          </component.Grid>
+        </AdListWraaper>
       )}
+      <component.Grid container>
+        <component.Grid item xs={6}>
+          <component.Button fullWidth color="primary">
+            이전 페이지
+          </component.Button>
+        </component.Grid>
+        <component.Grid item xs={6}>
+          <component.Button fullWidth color="primary">
+            다음 페이지
+          </component.Button>
+        </component.Grid>
+      </component.Grid>
     </MyProductsWrapper>
   );
 };
