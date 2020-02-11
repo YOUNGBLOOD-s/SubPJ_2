87e58c9292a2db1bc86b838ed99b2de7ae68987e
mandaringit <mandarin.dev@kakao.com>
2020-02-11 15:32:40 +0900

    api 변경으로 인한 수정 및 추가

--- MANAGE_FRONTEND/src/lib/api/ad.js
+++ MANAGE_FRONTEND/src/lib/api/ad.js
@@ -2,7 +2,9 @@ import client from './client';
 
 // 광고 목록 가져오기
 export const adlist = token =>
-  client.get('/api/man/nation/list', { headers: { Authorization: token } });
+  client.get('/api/man/nation/list?page=1', {
+    headers: { Authorization: token },
+  });
 
 export const remove_list = ({ token, idx }) =>
   client.delete(`/api/man/nation/delete/${idx}`, {
