4927e42979a291dab921ab5b44b4d45bc0449695
mandaringit <mandarin.dev@kakao.com>
2020-02-12 17:43:13 +0900

    S02P13C110-133 Nation 폼 레이아웃 잡기

--- MANAGE_FRONTEND/src/components/product/NationUpdateForm.js
+++ MANAGE_FRONTEND/src/components/product/NationUpdateForm.js
@@ -45,28 +45,12 @@ const NationUpdateForm = ({ nation }) => {
     <form>
       <component.Grid container spacing={1}>
         <component.Grid item xs={6}>
-          <StyledTextField
-            label="영문이름"
-            variant="outlined"
-            onChange={onChange}
-            value={form.en_name}
-            name="en_name"
-            autoComplete="en_name"
-            fullWidth
-          />
+          <DatePicker value={form.s_date} onChange={handleSdateChange} />
         </component.Grid>
         <component.Grid item xs={6}>
-          <StyledTextField
-            label="한글이름"
-            variant="outlined"
-            onChange={onChange}
-            value={form.ko_name}
-            name="ko_name"
-            autoComplete="ko_name"
-            fullWidth
-          />
+          <DatePicker value={form.f_date} onChange={handleFdateChange} />
         </component.Grid>
-        <component.Grid item xs={6}>
+        <component.Grid item xs={4}>
           <StyledTextField
             variant="outlined"
             select
@@ -84,53 +68,84 @@ const NationUpdateForm = ({ nation }) => {
             ))}
           </StyledTextField>
         </component.Grid>
-        <StyledTextField
-          label="TTS 문구"
-          variant="outlined"
-          onChange={onChange}
-          value={form.speech}
-          name="speech"
-          autoComplete="speech"
-          fullWidth
-        />
-        <StyledTextField
-          label="가격"
-          variant="outlined"
-          onChange={onChange}
-          value={form.price}
-          name="price"
-          autoComplete="price"
-          fullWidth
-        />
-        <DatePicker value={form.s_date} onChange={handleSdateChange} />
-        <DatePicker value={form.f_date} onChange={handleFdateChange} />
-        <StyledTextField
-          label="미세먼지"
-          variant="outlined"
-          onChange={onChange}
-          value={form.dust}
-          name="dust"
-          autoComplete="dust"
-          fullWidth
-        />
-        <StyledTextField
-          label="가중치"
-          variant="outlined"
-          onChange={onChange}
-          value={form.weight}
-          name="weight"
-          autoComplete="weight"
-          fullWidth
-        />
-        <StyledTextField
-          label="아이디"
-          variant="outlined"
-          onChange={onChange}
-          value={form.showcnt}
-          name="showcnt"
-          autoComplete="showcnt"
-          fullWidth
-        />
+        <component.Grid item xs={4}>
+          <StyledTextField
+            label="영문이름"
+            variant="outlined"
+            onChange={onChange}
+            value={form.en_name}
+            name="en_name"
+            autoComplete="en_name"
+            fullWidth
+          />
+        </component.Grid>
+        <component.Grid item xs={4}>
+          <StyledTextField
+            label="한글이름"
+            variant="outlined"
+            onChange={onChange}
+            value={form.ko_name}
+            name="ko_name"
+            autoComplete="ko_name"
+            fullWidth
+          />
+        </component.Grid>
+        <component.Grid item xs={12}>
+          <StyledTextField
+            label="TTS / 안내 문구"
+            variant="outlined"
+            onChange={onChange}
+            value={form.speech}
+            name="speech"
+            autoComplete="speech"
+            fullWidth
+            multiline
+          />
+        </component.Grid>
+        <component.Grid item xs={12}>
+          <StyledTextField
+            label="가격"
+            variant="outlined"
+            onChange={onChange}
+            value={form.price}
+            name="price"
+            autoComplete="price"
+            fullWidth
+          />
+        </component.Grid>
+        <component.Grid item xs={4}>
+          <StyledTextField
+            label="미세먼지 수치"
+            variant="outlined"
+            onChange={onChange}
+            value={form.dust}
+            name="dust"
+            autoComplete="dust"
+            fullWidth
+          />
+        </component.Grid>
+        <component.Grid item xs={4}>
+          <StyledTextField
+            label="가중치"
+            variant="outlined"
+            onChange={onChange}
+            value={form.weight}
+            name="weight"
+            autoComplete="weight"
+            fullWidth
+          />
+        </component.Grid>
+        <component.Grid item xs={4}>
+          <StyledTextField
+            label="광고노출횟수"
+            variant="outlined"
+            onChange={onChange}
+            value={form.showcnt}
+            name="showcnt"
+            autoComplete="showcnt"
+            fullWidth
+          />
+        </component.Grid>
       </component.Grid>
     </form>
   );
