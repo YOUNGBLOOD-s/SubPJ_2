69962e22fed12cbb41d8ed9fbce1804a4551388d
mandaringit <mandarin.dev@kakao.com>
2020-02-12 11:48:30 +0900

    S02P13C110-159 광고 READ_IMAGES 기본 구조 설정

--- MANAGE_FRONTEND/src/components/product/Images.js
+++ MANAGE_FRONTEND/src/components/product/Images.js
@@ -1,25 +1,46 @@
 import React from 'react';
 import styled from 'styled-components';
-
-const ImagesContainer = styled.div``;
+import component from '../../lib/material/component';
+import palette from '../../lib/styles/palette';
 
 const ImageWrapper = styled.div`
   display: flex;
+  flex-direction: column;
+  background-color: ${palette.grey[200]};
+  border-top-left-radius: 5px;
+  border-top-right-radius: 5px;
+  .image-text {
+    text-align: center;
+    font-weight: bold;
+    margin: 0.5rem 0;
+  }
+
+  .image {
+    max-height: 300px;
+    object-fit: cover;
+  }
 `;
+
+// type : 정도 (1-추움, 2-더움, 3-밝음, 4-어두움)
+const typeName = {
+  1: '추운 이미지',
+  2: '더운 이미지',
+  3: '밝은 이미지',
+  4: '어두운 이미지',
+};
 
 const Images = ({ images }) => {
   return (
-    <ImagesContainer>
-      <h4>온도별 이미지</h4>
+    <component.Grid container spacing={1}>
       {images.map(({ idx, nation, type, url }) => (
-        <ImageWrapper key={idx}>
-          <div>{idx}</div>
-          <div>{nation}</div>
-          <div>{type}</div>
-          <div>{url}</div>
-        </ImageWrapper>
+        <component.Grid item xs={12} sm={6}>
+          <ImageWrapper key={idx}>
+            <div className="image-text">{typeName[type]}</div>
+            <img className="image" src={url} alt={typeName[type]} />
+          </ImageWrapper>
+        </component.Grid>
       ))}
-    </ImagesContainer>
+    </component.Grid>
   );
 };
 
