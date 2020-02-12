c9c4308653270d27dfbaea0a8d568099f7af35a3
mandaringit <mandarin.dev@kakao.com>
2020-02-12 09:57:30 +0900

    컨텐츠 키 경고 수정

--- MANAGE_FRONTEND/src/components/product/Contents.js
+++ MANAGE_FRONTEND/src/components/product/Contents.js
@@ -3,17 +3,19 @@ import React from 'react';
 const Contents = ({ contents }) => {
   return (
     <div>
-      {contents.map(({ day, seq, title, detail, image, tofrom, transport }) => (
-        <div>
-          <div>{day}일차</div>
-          <div>{seq}번째</div>
-          <div>{title}</div>
-          <div>{image}</div>
-          <div>{detail}</div>
-          <div>{tofrom}</div>
-          <div>{transport}</div>
-        </div>
-      ))}
+      {contents.map(
+        ({ idx, day, seq, title, detail, image, tofrom, transport }) => (
+          <div key={idx}>
+            <div>{day}일차</div>
+            <div>{seq}번째</div>
+            <div>{title}</div>
+            <div>{image}</div>
+            <div>{detail}</div>
+            <div>{tofrom}</div>
+            <div>{transport}</div>
+          </div>
+        ),
+      )}
     </div>
   );
 };
