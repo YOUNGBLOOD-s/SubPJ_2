c6a4a29361703547d74f5458e3adf7531391548a
mandaringit <mandarin.dev@kakao.com>
2020-02-13 23:40:41 +0900

    Motn - 어드민이 아니면 수정 버튼 안보이게끔 수정

--- MANAGE_FRONTEND/src/components/product/Month.js
+++ MANAGE_FRONTEND/src/components/product/Month.js
@@ -4,7 +4,7 @@ import { useState } from 'react';
 import MonthForm from './MonthForm';
 import component from '../../lib/material/component';
 
-const Month = ({ month }) => {
+const Month = ({ month, user }) => {
   const { idx: id } = month;
   const [updating, setUpdating] = useState(false);
   return (
@@ -21,16 +21,18 @@ const Month = ({ month }) => {
           <component.Grid item xs={12}>
             <MonthTable datas={month} />
           </component.Grid>
-          <component.Grid item xs={12}>
-            <component.Button
-              variant="outlined"
-              color="primary"
-              fullWidth
-              onClick={() => setUpdating(true)}
-            >
-              수정
-            </component.Button>
-          </component.Grid>
+          {user && user.username === 'admin' && (
+            <component.Grid item xs={12}>
+              <component.Button
+                variant="outlined"
+                color="primary"
+                fullWidth
+                onClick={() => setUpdating(true)}
+              >
+                수정
+              </component.Button>
+            </component.Grid>
+          )}
         </>
       )}
     </component.Grid>
