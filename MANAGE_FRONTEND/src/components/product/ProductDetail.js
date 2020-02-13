367d54130101cd9d3cb6fd9c3caa10d05c23edff
mandaringit <mandarin.dev@kakao.com>
2020-02-13 12:05:52 +0900

    삭제 버튼 수정

--- MANAGE_FRONTEND/src/components/product/ProductDetail.js
+++ MANAGE_FRONTEND/src/components/product/ProductDetail.js
@@ -12,20 +12,20 @@ import MaterialCard from '../common/MaterialCard';
 import TitleBar from '../Detail/common/TitleBar';
 import CaptionText from '../Detail/common/CaptionText';
 import LoadingBackdrop from '../common/LoadingBackdrop';
-import { remove_list } from '../../lib/api/ad';
-import { removeAd } from '../../modules/ads';
+import { removeList } from '../../lib/api/ad';
 import component from '../../lib/material/component';
 import DeleteAlertDialog from '../common/DeleteAlertDialog';
 
 const DetailContainer = styled.div`
   padding: 1rem;
 `;
+
 const DetailWrapper = styled.div`
   max-width: 1000px;
   margin: 0 auto;
 `;
 
-const ProductDetail = ({ match }) => {
+const ProductDetail = ({ match, history }) => {
   const { id } = match.params;
   const token = sessionStorage.getItem('access_token');
   const dispatch = useDispatch();
@@ -44,8 +44,8 @@ const ProductDetail = ({ match }) => {
   const onRemoveAd = async () => {
     try {
       const token = sessionStorage.getItem('access_token');
-      await remove_list({ token, id });
-      dispatch(removeAd(id));
+      await removeList({ token, id });
+      history.push('/admin');
     } catch (e) {
       console.log(e);
     }
@@ -56,43 +56,65 @@ const ProductDetail = ({ match }) => {
       {!loading && product ? (
         <DetailWrapper>
           {/* OWNER */}
-          <TitleBar>광고 소유자</TitleBar>
-          <CaptionText>광고를 소유하고 있는 소유자의 정보입니다.</CaptionText>
-          <MaterialCard>
-            <Owner owner={product.owner} />
-          </MaterialCard>
+          {product.owner && (
+            <>
+              <TitleBar>광고 소유자</TitleBar>
+              <CaptionText>
+                광고를 소유하고 있는 소유자의 정보입니다.
+              </CaptionText>
+              <MaterialCard>
+                <Owner owner={product.owner} />
+              </MaterialCard>
+            </>
+          )}
 
           {/* NATION */}
-          <TitleBar>나라 기본 설정</TitleBar>
-          <CaptionText>광고중인 나라의 기본설정입니다.</CaptionText>
-          <MaterialCard>
-            <Nation nation={product.nation} />
-          </MaterialCard>
+          {product.nation && (
+            <>
+              <TitleBar>나라 기본 설정</TitleBar>
+              <CaptionText>광고중인 나라의 기본설정입니다.</CaptionText>
+              <MaterialCard>
+                <Nation nation={product.nation} />
+              </MaterialCard>
+            </>
+          )}
 
           {/* IMAGES */}
-          <TitleBar>온도별 나라 대표이미지</TitleBar>
-          <CaptionText>
-            춥고, 더울때 보여줄 나라의 대표 이미지입니다.
-          </CaptionText>
-          <MaterialCard>
-            <Images images={product.images} />
-          </MaterialCard>
+          {product.images && (
+            <>
+              <TitleBar>온도별 나라 대표이미지</TitleBar>
+              <CaptionText>
+                춥고, 더울때 보여줄 나라의 대표 이미지입니다.
+              </CaptionText>
+              <MaterialCard>
+                <Images images={product.images} />
+              </MaterialCard>
+            </>
+          )}
 
           {/* ROUTES/CONTENTS */}
-          <TitleBar>나라의 일자별 경로</TitleBar>
-          <CaptionText>광고중인 나라의 N일차 M번째 경로입니다.</CaptionText>
-          <MaterialCard>
-            <Contents contents={product.contents} />
-          </MaterialCard>
+          {product.contents && (
+            <>
+              <TitleBar>나라의 일자별 경로</TitleBar>
+              <CaptionText>광고중인 나라의 N일차 M번째 경로입니다.</CaptionText>
+              <MaterialCard>
+                <Contents contents={product.contents} />
+              </MaterialCard>
+            </>
+          )}
 
           {/* MONTH */}
-          <TitleBar>나라의 온/습도 테이블</TitleBar>
-          <CaptionText>
-            광고중인 나라의 기본 참고 온/습도 테이블입니다.
-          </CaptionText>
-          <MaterialCard>
-            <Month month={product.month} />
-          </MaterialCard>
+          {product.month && (
+            <>
+              <TitleBar>나라의 온/습도 테이블</TitleBar>
+              <CaptionText>
+                광고중인 나라의 기본 참고 온/습도 테이블입니다.
+              </CaptionText>
+              <MaterialCard>
+                <Month month={product.month} />
+              </MaterialCard>
+            </>
+          )}
 
           {/* DELETE */}
           {user && user.username === 'admin' && (
