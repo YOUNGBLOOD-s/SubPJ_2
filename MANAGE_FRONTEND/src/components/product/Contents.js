f1efb0f77ff3e8df066c235bbbae271f2eb597b6
mandaringit <mandarin.dev@kakao.com>
2020-02-13 23:45:09 +0900

    ADMIN이 아니면 컨텐츠 수정 버튼 안보이도록 수정

--- MANAGE_FRONTEND/src/components/product/Contents.js
+++ MANAGE_FRONTEND/src/components/product/Contents.js
@@ -2,12 +2,12 @@ import React from 'react';
 import component from '../../lib/material/component';
 import ContentItem from './ContentItem';
 
-const Contents = ({ contents }) => {
+const Contents = ({ contents, user }) => {
   return (
     <component.Grid container spacing={1}>
       {contents.map(content => (
         <component.Grid item xs={12} key={content.idx}>
-          <ContentItem content={content} />
+          <ContentItem content={content} user={user} />
         </component.Grid>
       ))}
     </component.Grid>
