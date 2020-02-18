61a3dfbcc09872846fa3d4e1c2a29c4697124214
mandaringit <mandarin.dev@kakao.com>
2020-02-19 08:57:39 +0900

    Nation Add Confilic 에러 메세지 추가

--- MANAGE_FRONTEND/src/components/manage/productManage/AddProduct/NationAddForm.js
+++ MANAGE_FRONTEND/src/components/manage/productManage/AddProduct/NationAddForm.js
@@ -127,7 +127,21 @@ const NationAddForm = ({ classes, steps, step }) => {
           dispatch(selectNation(nationidx));
           dispatch(nextStep());
         })
-        .catch(err => console.log(err));
+        .catch(err => {
+          const { status } = err.response;
+          if (status === 409) {
+            setErrors({
+              ...errors,
+              en_name: true,
+              ko_name: true,
+            });
+            setErrorText({
+              ...errorText,
+              ko_name: '중복된 한글 국가명입니다.',
+              en_name: '중복된 영문 국가명입니다.',
+            });
+          }
+        });
     }
   };
 
