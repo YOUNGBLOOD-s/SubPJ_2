74704663e34a30d8d96da29f666563df1669764a
mandaringit <mandarin.dev@kakao.com>
2020-02-18 14:01:02 +0900

    로그인/가입 폼 검증

--- MANAGE_FRONTEND/src/containers/auth/LoginForm.js
+++ MANAGE_FRONTEND/src/containers/auth/LoginForm.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import AuthForm from '../../components/auth/AuthForm';
 import { changeField, initializeForm, login } from '../../modules/auth';
@@ -7,12 +7,17 @@ import { check } from '../../modules/user';
 
 const LoginForm = ({ history }) => {
   const dispatch = useDispatch();
-  const { form, auth, authError, user } = useSelector(({ auth, user }) => ({
-    form: auth.login,
-    auth: auth.auth,
-    authError: auth.authError,
-    user: user.user,
-  }));
+  const { form, auth, authError, user, loading } = useSelector(
+    ({ auth, user, loading }) => ({
+      form: auth.login,
+      auth: auth.auth,
+      authError: auth.authError,
+      user: user.user,
+      loading: loading['auth/LOGIN'],
+    }),
+  );
+
+  const [error, setError] = useState(null);
 
   const onChange = e => {
     const { value, name } = e.target;
@@ -24,6 +29,12 @@ const LoginForm = ({ history }) => {
     const { username, password } = form;
 
     if (!username || !password) {
+      setError('아이디 또는 비밀번호를 입력해주세요.');
+      return;
+    }
+
+    if (username.length > 50 || password.length > 50) {
+      setError('잘못된 아이디 또는 비밀번호 입력입니다.');
       return;
     }
 
@@ -37,13 +48,18 @@ const LoginForm = ({ history }) => {
 
   useEffect(() => {
     if (authError) {
-      console.log('로그인 오류 발생');
-      console.log(authError);
+      const { status } = authError.response;
+      if (status === 401) {
+        setError('아이디 또는 비밀번호가 올바르지 않습니다.');
+      }
+
+      if (status === 404) {
+        setError('존재하지 않는 사용자입니다.');
+      }
+
       return;
     }
     if (auth) {
-      console.log('로그인 성공');
-      console.log(auth);
       dispatch(check(auth.token));
       sessionStorage.setItem('access_token', auth.token);
     }
@@ -66,7 +82,8 @@ const LoginForm = ({ history }) => {
       form={form}
       onChange={onChange}
       onSubmit={onSubmit}
-      error={authError}
+      error={error}
+      loading={loading}
     />
   );
 };
