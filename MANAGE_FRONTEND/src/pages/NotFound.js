8fad91d129998cc7ef6690f0ce550214b707fa75
mandaringit <mandarin.dev@kakao.com>
2020-02-19 15:34:51 +0900

    Not Found 스타일링, MyPage 스타일링

--- MANAGE_FRONTEND/src/pages/NotFound.js
+++ MANAGE_FRONTEND/src/pages/NotFound.js
@@ -1,7 +1,53 @@
 import React from 'react';
+import styled from 'styled-components';
+import palette from '../lib/styles/palette';
+import { Link } from 'react-router-dom';
+
+const NotFoundWrapper = styled.div`
+  flex-grow: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  font-family: 'Handon3gyeopsal600g';
+  padding: 1rem;
+  .error {
+    font-size: 3rem;
+    color: ${palette.red[600]};
+    margin-bottom: 0.5rem;
+    font-weight: bold;
+  }
+
+  .text {
+    font-size: 3rem;
+    margin-bottom: 1rem;
+    @media (max-width: 600px) {
+      font-size: 2rem;
+    }
+  }
+`;
+
+const StyledLink = styled(Link)`
+  margin-top: 2rem;
+  font-size: 2rem;
+  color: ${palette.theme[200]};
+  text-decoration: underline;
+`;
 
 const NotFound = () => {
-  return <div>찾을 수 없는 페이지 ㅎ</div>;
+  return (
+    <NotFoundWrapper>
+      <div className="error">404</div>
+      <div className="error">NOT FOUND</div>
+      <div className="text">
+        <span role="img" aria-label="img">
+          🤔
+        </span>{' '}
+        페이지를 찾을 수 없습니다!
+      </div>
+      <StyledLink to="/">메인페이지로</StyledLink>
+    </NotFoundWrapper>
+  );
 };
 
 export default NotFound;
